503ef2a90197d2396e526bb3cf28a631fd4800e3
ferreirawill05 <william.ferreira@outlook.com.br>
2023-11-02 18:01:21 -0300

    all done

--- src/Components/FooterDiv/Footer.jsx
+++ src/Components/FooterDiv/Footer.jsx
@@ -1,10 +1,80 @@
 import React from "react";
+import insta from "../../assets/instagram (2).png"
+import face from "../../assets/facebook (2).png"
+import x from "../../assets/icons8-twitterx-50.png"
 
 const Footer = () => {
 
   return(
-    <div>Footer</div>
-  )
+    <div className="footer p-[5rem] mb-4 bg-blueColor rounded-[10px] gap-8 grid grid-cols-5 m-auto items-center justify-center">
+
+      <div>
+        <div className="logoDiv">
+            <h1 className="logo text-[25px] pb-[1.5rem] text-white"><strong>Un</strong>Employed</h1>
+        </div>
+        <p className="text-white pb-[13px] opacity-70 leading-7">
+          We always make out seekers and companies find the best jobs and employers find the best candidates.
+        </p>
+      </div>
+
+      <div className="grid">
+        <span className="divTittle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Company
+        </span>
+        <div className="grid gap-3">
+          <li className="text-white opacity-[.7] hover:opacity-[1]">About Us</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Features</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">News</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">FAQ</li>
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTittle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Resources
+        </span>
+        <div className="grid gap-3">
+
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Account</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Support Center</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Feedback</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Contact Us</li>
+
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTittle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Support
+        </span>
+        <div className="grid gap-3">
+
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Events</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Promo</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Req Demo</li>
+          <li className="text-white opacity-[.7] hover:opacity-[1]">Carrers</li>
+
+        </div>
+      </div>
+
+      <div className="grid">
+        <span className="divTittle text-[18px] font-semibold pb-[1.5rem] text-white">
+          Contact Info
+        </span>
+        <div>
+          <small className="text-[14px] text-white">
+            will@email.com
+          </small>
+          <div className="icons flex gap-4 py-[1rem]">
+            <img src={insta} alt="" className="bg-white p-[5px] w-[35px] h-[35px] rounded-full icon"/>
+            <img src={face} alt="" className="bg-white p-[5px] h-[35px] w-[35px] rounded-full icon"/>
+            <img src={x} alt="" className="bg-white p-[5px] h-[35px] w-[35px] rounded-full icon"/>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  ) 
 }
 
 export default Footer
--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -8,13 +8,13 @@ const Value = () => {
 
     return(
         <div className="mb-[4rem] mt-[6rem]">
-            <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[560px] block">The value that holds us true and to account</h1>
-            <div className="grip flex gap-[19rem] grid-cols-3 items-center">
+            <h1 className="text-textColor text-[25px] py-[2rem] pb-[3rem] font-bold w-[400px] block">The value that holds us true and to account</h1>
+            <div className="grip flex gap-[10rem] grid-cols-3 items-center gap">
 
                 <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
                     <div className="flex items-center gap-3">
-                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
-                            <img src={logo0} alt="" className="w-[70%]" />
+                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f7edf5] h-[40px] w-[40px] flex items-center justify-center">
+                            <img src={logo2} alt="" className="w-[70%]" />
                         </div>
 
                         <span className="font-semibold text-textColor text-[18px]">
@@ -26,10 +26,10 @@ const Value = () => {
                     </p>
                 </div>
 
-                <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+                <div className="singleGrid rounded-[10px] hover:bg-[#f7edf5] p-[1.5rem]">
                     <div className="flex items-center gap-3">
-                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
-                            <img src={logo2} alt="" className="w-[70%]" />
+                        <div className="imgDiv p-[4px] rounded-[.8rem] bg-[#f7d1e1] h-[40px] w-[40px] flex items-center justify-center">
+                            <img src={logo0} alt="" className="w-[70%]" />
                         </div>
 
                         <span className="font-semibold text-textColor text-[18px]">
@@ -37,13 +37,13 @@ const Value = () => {
                         </span>
                     </div>
                     <p className="text-[13px] text-textColor opacity-[.7] py-[1rem] font-semibold">
-                        Things being made beautiful simple are at ht eheart of everything we do.
+                        We believe in making things better for everyone, even if just by a little bit!
                     </p>
                 </div>
 
-                <div className="singleGrid rounded-[10px] hover:bg-[#eeedf7] p-[1.5rem]">
+                <div className="singleGrid rounded-[10px] hover:bg-[#fcfae3] p-[1.5rem]">
                     <div className="flex items-center gap-3">
-                        <div className="imgDiv p-[1px] rounded-[.8rem] bg-[#dedef8] h-[40px] w-[40px] flex items-center justify-center">
+                        <div className="imgDiv p-[1px] rounded-[.8rem] bg-[#f3f2ad] h-[40px] w-[40px] flex items-center justify-center">
                             <img src={logo3} alt="" className="w-[70%]" />
                         </div>
 
@@ -57,6 +57,19 @@ const Value = () => {
                 </div>
                 
             </div>
+
+            <div className="card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded-[10px]">
+
+                <div>
+                    <h1 className="text-blueColor text-[30px] font-bold">Ready to switch a carrer?</h1>
+                    <h1 className="text-textColor text-[25px] font-bold">Let's get started</h1>
+                </div>
+                    <button className="border-[2px] rounded-[10px] py-[4px] px-[30px] text-[18px] font-semibold text-blueColor hover:bg-white border-blueColor">
+                        Get started
+                    </button>
+
+            </div>
+
         </div>
     )
 }
